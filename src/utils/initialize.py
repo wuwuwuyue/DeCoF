ea7bb9fe10fa84e7572eeeec7ce5ad407d4d65a8
longm <96729923+wuwuwuyue@users.noreply.github.com>
2025-04-20 18:09:32 +0800

    Update initialize.py

--- src/utils/initialize.py
+++ src/utils/initialize.py
@@ -12,8 +12,9 @@ def init_ff(phase,data_name='text2video_zero',n_frames=8):
 	#print(len(folder_list_r))
 	dataset_path_f=os.path.join('/home/Text2Video-Zero-main/ours/data/',phase,data_name,'1_fake/')
 	folder_list_f = sorted(glob(dataset_path_f+'*'))
+	label_list=[0]*len(folder_list_r)+[1]*len(folder_list_f)
 
-	return folder_list_r,folder_list_f
+	return folder_list_r+folder_list_f,label_list
 	
 
 	
